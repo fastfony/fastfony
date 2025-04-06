48150282eb520618ee0941d6e8728ed18efb0b87
Mathieu Dumoutier <13251143+neothone@users.noreply.github.com>
2025-04-06 17:27:47 +0200

    fix: add CI and fix install step 3 & roles matrix

--- src/Pro/Controller/Security/Login.php
+++ src/Pro/Controller/Security/Login.php
@@ -15,7 +15,7 @@ class Login extends AbstractController
      * @return array<string, mixed>
      */
     #[Route('/login', name: 'login')]
-    #[Template('security/login.html.twig')]
+    #[Template('pro/security/login.html.twig')]
     public function __invoke(AuthenticationUtils $authenticationUtils): array
     {
         return [
