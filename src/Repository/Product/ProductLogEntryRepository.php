ee7718bd44c75bf3ccb2112462a84458a7656db0
Mathieu Dumoutier <13251143+neothone@users.noreply.github.com>
2025-06-26 20:30:34 +0200

    fix: phpstan error

--- src/Repository/Product/ProductLogEntryRepository.php
+++ src/Repository/Product/ProductLogEntryRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repository\Product;
 
-use App\Pro\Entity\Product\ProductLogEntry;
+use App\Entity\Product\ProductLogEntry;
 use App\Repository\SaveAndRemoveMethod;
 use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
 
