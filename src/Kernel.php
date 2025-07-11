9aacd6632fe7879d6caaa1409d355ece6793ad3b
Mathieu Dumoutier <13251143+neothone@users.noreply.github.com>
2025-07-11 11:22:04 +0200

    chore: upgrade to SF 7.3.1 and change version to 0.3.1-RC3

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,12 +10,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const FF_VERSION = '0.3.0-RC2';
-    public const FF_VERSION_ID = 00300;
+    public const FF_VERSION = '0.3.1-RC3';
+    public const FF_VERSION_ID = 00301;
     public const FF_MAJOR_VERSION = 0;
     public const FF_MINOR_VERSION = 3;
-    public const FF_RELEASE_VERSION = 0;
-    public const FF_EXTRA_VERSION = 'RC2';
+    public const FF_RELEASE_VERSION = 1;
+    public const FF_EXTRA_VERSION = 'RC3';
 
     public const FF_END_OF_MAINTENANCE = '03/2026';
     public const FF_END_OF_LIFE = '03/2026';
