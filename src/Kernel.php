2eb64265048c22cbdc75e9716c84425414722006
Mathieu Dumoutier <13251143+neothone@users.noreply.github.com>
2025-07-20 23:13:26 +0200

    chore: change version number to 0.4.0-DEV

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,13 +10,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const FF_VERSION = '0.3.1';
-    public const FF_VERSION_ID = 00301;
+    public const FF_VERSION = '0.4.0';
+    public const FF_VERSION_ID = 00400;
     public const FF_MAJOR_VERSION = 0;
-    public const FF_MINOR_VERSION = 3;
-    public const FF_RELEASE_VERSION = 1;
-    public const FF_EXTRA_VERSION = '';
+    public const FF_MINOR_VERSION = 4;
+    public const FF_RELEASE_VERSION = 0;
+    public const FF_EXTRA_VERSION = 'DEV';
 
-    public const FF_END_OF_MAINTENANCE = '03/2026';
-    public const FF_END_OF_LIFE = '03/2026';
+    public const FF_END_OF_MAINTENANCE = '01/2029';
+    public const FF_END_OF_LIFE = '01/2030';
 }
