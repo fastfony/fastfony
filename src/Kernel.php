2fb72bb49ae0fdd3749677194437efb2d91fef60
neothone (Mathieu Dumoutier) <13251143+neothone@users.noreply.github.com>
2025-08-29 16:14:08 +0200

    chore: upgrade to SF 7.3.3 and others upgrades

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,11 +10,11 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const FF_VERSION = '0.3.2';
-    public const FF_VERSION_ID = 00302;
+    public const FF_VERSION = '0.3.3';
+    public const FF_VERSION_ID = 00303;
     public const FF_MAJOR_VERSION = 0;
     public const FF_MINOR_VERSION = 3;
-    public const FF_RELEASE_VERSION = 2;
+    public const FF_RELEASE_VERSION = 3;
     public const FF_EXTRA_VERSION = '';
 
     public const FF_END_OF_MAINTENANCE = '03/2026';
