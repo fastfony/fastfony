4163e604cf9ab2e4beda1e16fb8747140929ae74
neothone (Mathieu Dumoutier) <13251143+neothone@users.noreply.github.com>
2025-10-08 20:49:08 +0200

    WIP

--- tests/Functional/Admin/UserCrudTest.php
+++ tests/Functional/Admin/UserCrudTest.php
@@ -24,4 +24,35 @@ final class UserCrudTest extends WebTestCase
         );
         $this->assertEmailCount(1);
     }
+
+    public function testSwitchUser(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['enabled' => true]);
+        $otherUser = $userRepository->findOneBy(['enabled' => true, 'id' => $user->getId() + 1]);
+
+        self::ensureKernelShutdown();
+
+        $client = static::createClient();
+        $client->loginUser($user);
+
+        // TODO : pour le moment ça passe ici
+        if (null === $otherUser) {
+            $this->markTestSkipped('No other enabled user found for switchUser test.');
+        }
+
+        $client->request(
+            'GET',
+            '/admin/user-crud/' . $otherUser->getId() . '/switch-user',
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isRedirect(), 'Response should be a redirect');
+        $this->assertStringContainsString(
+            (string) $otherUser->getUserIdentifier(),
+            (string) $response->headers->get('location'),
+            'Redirect location should contain switched user identifier'
+        );
+    }
 }
