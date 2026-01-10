1056cde3b0772968bbde136d7703acbc1a796f14
neothone (Mathieu Dumoutier) <13251143+neothone@users.noreply.github.com>
2026-01-10 21:18:26 +0100

    chore: upgrade to Symfony 7.4

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,8 +10,8 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const FF_VERSION = '0.4.0';
-    public const FF_EXTRA_VERSION = 'DEV';
+    public const FF_VERSION = '0.4.3';
+    public const FF_EXTRA_VERSION = '';
 
     public const FF_END_OF_MAINTENANCE = '01/2029';
     public const FF_END_OF_LIFE = '01/2030';
